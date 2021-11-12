1f9b3460004bd9ea94bcb8b7cb5e4fd633f28d97
Laura Meister <laura.meister@student.hpi.uni-potsdam.de>
2021-11-12 15:30:57 +0100

    test for #5

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
  describe "New author page", type: :feature do
-    
+
    it "should exist at 'new_author_path' and render withour error" do
      # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
      visit new_author_path
@@ -15,4 +15,17 @@ require 'rails_helper'
     expect(page).to have_field('author[homepage]')
    end
   
+   it "should save a newly submitted author in the database" do
+    visit new_author_path
+    local_first_name = 'Grace'
+    local_last_name = 'Hopper'
+    local_homepage = 'https://en.wikipedia.org/wiki/Grace_Hopper'
+    page.fill_in 'author[first_name]', with: local_first_name
+    page.fill_in 'author[last_name]', with: local_last_name
+    page.fill_in 'author[homepage]', with: local_homepage
+    find('input[type="submit"]').click
+    Author.find_by!(first_name: local_first_name, last_name: local_last_name, homepage: local_homepage)
+   end
+
+
  end
