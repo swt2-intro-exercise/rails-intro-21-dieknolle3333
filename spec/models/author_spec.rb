09667b077964dbe377d6a9b72f40358c9aa0a83e
Laura Meister <laura.meister@student.hpi.uni-potsdam.de>
2021-11-12 14:54:08 +0100

    fixes #4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,15 +1,15 @@
 describe "Author", type: :model do
 
-it "should have a first name, a last name and a homepage (all three strings) and a name method returning first name + last name" do
-   local_first_name = 'Sandra'
-   local_last_name = 'Ciesek'
-   local_homepage = 'https://www.dzif.de/en/node/2284'
-
-   author = Author.new(:first_name => local_first_name, :last_name => local_last_name, :homepage => local_homepage)
-   expect(author.first_name).to eq(local_first_name)
-   expect(author.last_name).to eq(local_last_name)
-   expect(author.homepage).to eq(local_homepage)
-   expect(author.name).to eq(local_first_name + " " + local_last_name)
-   end
-
-end
+    it "should have a first name, a last name and a homepage (all three strings) and a name method returning first name + last name" do
+       local_first_name = 'Sandra'
+       local_last_name = 'Ciesek'
+       local_homepage = 'https://www.dzif.de/en/node/2284'
+    
+       author = Author.new(:first_name => local_first_name, :last_name => local_last_name, :homepage => local_homepage)
+       expect(author.first_name).to eq(local_first_name)
+       expect(author.last_name).to eq(local_last_name)
+       expect(author.homepage).to eq(local_homepage)
+       expect(author.name).to eq(local_first_name + " " + local_last_name)
+       end
+    
+    end
