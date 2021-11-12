201ff0af5de41dc24baf72b24bb97a77e51c7e0a
Laura Meister <laura.meister@student.hpi.uni-potsdam.de>
2021-11-12 14:08:16 +0100

    fixes issue #2

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get 'welcome/index'
+  resources :author
+
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   
