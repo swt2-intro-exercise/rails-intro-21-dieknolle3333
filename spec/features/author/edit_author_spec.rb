5ca2e81321754ad18c308518d039240076ec00c2
Laura Meister <laura.meister@student.hpi.uni-potsdam.de>
2021-11-20 20:27:04 +0100

    solves #9

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
  describe "Edit author page", type: :feature do
  
-    it "should display an existing author's details on a show page" do
+    it "should store updated information about an author in the database" do
         edited_first_name = "Grace THE QUEEN"
         @grace = FactoryBot.create :author
         visit edit_author_path(@grace)
         page.fill_in 'author[first_name]', with: edited_first_name
         find('input[type="submit"]').click
         @grace.reload
-        expect(@grace).to have_attribute(first_name: edited_first_name)
+        Author.find_by!(first_name: edited_first_name)
+        expect(@grace).to have_attributes(first_name: edited_first_name)
     end
 
  end
