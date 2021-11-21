8470214a379a14d179d93ac08b4831ecf49b8435
Laura Meister <laura.meister@student.hpi.uni-potsdam.de>
2021-11-21 23:01:30 +0100

    tests #10

--- spec/features/author/overview_spec.rb
+++ spec/features/author/overview_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
  describe "Overview page", type: :feature do
 
-   it "should exist at 'new_author_path' and render withour error" do
+   it "should exist at 'authors_path' and render withour error" do
      visit authors_path
      expect(page).to have_content("Name")
      expect(page).to have_content("Homepage")
@@ -11,8 +11,16 @@ require 'rails_helper'
          expect(page).to have_content author.name
          expect(page).to have_content author.homepage
          expect(page).to have_link 'Show', href: author_path(author)
+         expect(page).to have_link 'Delete', href: author_path(author)
      end
    end
+
+   it "should have a link to delete an author for every entry" do
+    visit authors_path
+    init_count = Author.count
+    find('a[data-method=\'delete\'][href=\'/authors/1\']').click
+    expect(Author.count).to == init_count - 1  
+  end   
 
 
  end
