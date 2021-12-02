f912874169b84e8f5a7b1110ab12b356e99ab8dc
Laura Meister <laura.meister@student.hpi.uni-potsdam.de>
2021-12-02 22:04:02 +0100

    fixes #15

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
  describe "Edit paper page", type: :feature do
 
-    it "should display the paper's authors' full names" do
+    it "should have a multiselect box for the paper's authors" do
         @paper = FactoryBot.create :paper
-        visit paper_path(@paper)
-        expect(page).to have_css("select multiple")
+        visit edit_paper_path(@paper)
+        expect(page).to have_css("select[multiple]")
     end
 
  end
